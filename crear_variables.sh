2bf4b3a28f33055f1aee342857e782e9ca822b95
Eduardo Manuel Palacios Tebalán <manuelpalacios7@gmail.com>
2021-02-04 10:29:22 -0600

    Corregir variable de entorno.

--- crear_variables.sh
+++ crear_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties userbd=userbd
 ./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties passwordbd=password
-./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties bd_ingenieria2_url=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
+./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties urlbd=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
