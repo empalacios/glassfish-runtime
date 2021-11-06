561b23c973b2394f966efcd6107cc2485156f613
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-11-05 21:46:17 -0600

    Ejecutar el comando asadmin con su path absoluto

--- crear_variables.sh
+++ crear_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties userbd=userbd
-./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties passwordbd=password
-./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties urlbd=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
+bash /opt/glassfish4/glassfish/bin/asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties userbd=userbd
+bash /opt/glassfish4/glassfish/bin/asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties passwordbd=password
+bash /opt/glassfish4/glassfish/bin/asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties urlbd=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
