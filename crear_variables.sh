0144462ee653bd38ee262af976fcfe9c2e8560e8
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-11-05 21:40:48 -0600

    Resolver la pregunta para aceptar el certificado de la consola de administración en creación de variables de entorno

--- crear_variables.sh
+++ crear_variables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties userbd=userbd
-./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties passwordbd=password
-./asadmin --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties urlbd=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
+./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties userbd=userbd
+./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties passwordbd=password
+./asadmin --interactive=false --user=admin --passwordfile=/home/vagrant/glassfishpwd --port 4848 create-system-properties urlbd=jdbc\\:postgresql\\://localhost\\:5432/mydatabase
